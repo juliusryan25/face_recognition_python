866721954262972b50f22564dc5ff5017ec5ef40
Daffa Bagus <daffabagus031@gmail.com>
2024-05-20 13:32:00 +0700

    Mengupload hasil capture sistem ke database, Mengubah data image menjadi binary dan sebaliknya, Menampilkan hasil capture yang baru masuk ke database

--- package/__init__.py
+++ package/__init__.py
@@ -7,5 +7,9 @@ import psycopg2
 import os
 import io
 from package.database.koneksi import get_connection
-from package.database.query import rows,conn,cur
-from PIL import Image
+from package.database.query import rows,conn,cur, upload_to_database
+import tkinter as tk
+from PIL import Image, ImageTk
+from io import BytesIO
+import base64
+from package.binary_image.image_display import show_binary_image, get_binary_data_from_database
