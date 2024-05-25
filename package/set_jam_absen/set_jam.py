2a21b0ae3c3b790b07d9af6abd4f24be8fb823b4
juliusryan25 <ryanjulius742@gmail.com>
2024-05-25 10:00:15 +0700

    Mengatur absen otomatis

--- package/set_jam_absen/set_jam.py
+++ package/set_jam_absen/set_jam.py
@@ -1,7 +1,7 @@
 from package import datetime
 
-absen_masuk_start = datetime.now().replace(hour=6, minute=0, second=0, microsecond=0)
-absen_masuk_end = datetime.now().replace(hour=10, minute=0, second=0, microsecond=0)
-absen_pulang_start = datetime.now().replace(hour=10, minute=1, second=0, microsecond=0)
-absen_pulang_end = datetime.now().replace(hour=5, minute=59, second=0, microsecond=0)
+absen_masuk_start = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+absen_masuk_end = datetime.now().replace(hour=9, minute=57, second=0, microsecond=0)
+absen_pulang_start = datetime.now().replace(hour=9, minute=57, second=30, microsecond=0)
+absen_pulang_end = datetime.now().replace(hour=11, minute=0, second=0, microsecond=0)
 
