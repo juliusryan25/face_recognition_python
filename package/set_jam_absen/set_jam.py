9207b6549c712349e7abe5713cfa8a0279ac4891
juliusryan25 <ryanjulius742@gmail.com>
2024-05-25 13:03:16 +0700

    set_time

--- package/set_jam_absen/set_jam.py
+++ package/set_jam_absen/set_jam.py
@@ -1,7 +1,7 @@
 from package import datetime
 
 absen_masuk_start = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
-absen_masuk_end = datetime.now().replace(hour=9, minute=57, second=0, microsecond=0)
-absen_pulang_start = datetime.now().replace(hour=9, minute=57, second=30, microsecond=0)
+absen_masuk_end = datetime.now().replace(hour=10, minute=24, second=0, microsecond=0)
+absen_pulang_start = datetime.now().replace(hour=10, minute=24, second=30, microsecond=0)
 absen_pulang_end = datetime.now().replace(hour=11, minute=0, second=0, microsecond=0)
 
